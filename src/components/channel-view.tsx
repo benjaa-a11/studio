84a6e1930ffb6ffb8a827315bf3456225bf2739b
Benjamin Ferrer <benjabostero223@gmail.com>
2025-06-23 23:18:28 +0000

    bien, quiero que trabajemos muy profesionalmente en esta app web, en su

--- src/components/channel-view.tsx
+++ src/components/channel-view.tsx
@@ -165,6 +165,7 @@ export default function ChannelView({ channel, relatedChannels }: ChannelViewPro
           src={currentStreamUrl}
           title={channel.name}
           onLoad={handleIframeLoad}
+          sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-presentation allow-modals"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
