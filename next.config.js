72e7b40b640d2fe057339a9b03fe20c8c22e7866
Benjamin Ferrer <benjabostero223@gmail.com>
2025-07-05 23:25:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -45,6 +45,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'i.imgur.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
     ],
   },
 };
