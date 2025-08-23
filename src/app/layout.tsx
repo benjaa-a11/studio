6db44919c80dd9ee50697f7c1bf0016bcd3954d8
Benjamin Ferrer <benjabostero223@gmail.com>
2025-08-23 05:21:56 +0000

    bueno, por ultimo quiero que creemos una proteccion de privacidad y segu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default async function RootLayout({
     <html lang="es" suppressHydrationWarning>
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased flex flex-col",
+          "min-h-screen bg-background font-sans antialiased flex flex-col secure-app-body",
           fontInter.variable,
           fontPoppins.variable
         )}
